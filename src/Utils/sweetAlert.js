3ffc7da961a60c35b9aabf5c528eb94a18dcad98
spancavil <soancavil@gmail.com>
2022-03-08 12:13:59 -0300

    Modified message on error

--- src/Utils/sweetAlert.js
+++ src/Utils/sweetAlert.js
@@ -2,10 +2,10 @@ import Swal from 'sweetalert2';
 
 export default function SwalAlert (error) {
     Swal.fire({
-        title: "Ocurrió un error",
-        text: error,
+        html: '<b>Hubo un problema de conexión</b><br/><br/>' +
+        '¡Lo sentimos! Si lo necesitas, puedes contactarte con nuestro <strong>chat de ayuda.</strong>',
         icon: 'error',
         showConfirmButton: true,
-        width: '400px'
+        width: '470px',
     })
 }
