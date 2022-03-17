08e402afb75bfc12b5fa421b65c6d9930f00f6d5
spancavil <soancavil@gmail.com>
2022-03-17 09:00:11 -0300

    Fix on route

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Impresion/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/definir-parametros" element={<DefineParams />} />
         <Route path="/impresion" element={<Impresion />} />
         <Route path="/error-pdf" element={<ErrorPdf />} />
