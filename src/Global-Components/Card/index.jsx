39259698b4d6365ad460222f58f0db1828c69a76
spancavil <soancavil@gmail.com>
2022-07-20 22:01:12 -0300

    Card delivery changed text

--- src/Global-Components/Card/index.jsx
+++ src/Global-Components/Card/index.jsx
@@ -179,7 +179,7 @@ const Card = ({type = "weight", content, onClick, block, setBlock, handleEdit = 
                     />
                 <h2 className={styles.title}>{Object.keys(content)[0].includes("OXX") ? "Sí, entregar en esta tienda": "No, entregar en otro lugar" }</h2>
                 <h3 className={styles.sub}>{Object.keys(content)[0].includes("OXX") ? "El costo de comisión es de $7 MXN por paquete.": "Sin costo de comisión."}</h3>
-                <h3 className={styles.text}>{Object.keys(content)[0].includes("OXX") ? "Entrega tu envío en la caja para que la paquetería que elegiste pueda pasar a buscarlo." : "Acude a una sucursal Skydropx o de la paquetería que elegiste. No podrás dejar tu paquete en esta tienda."}</h3>
+                <h3 className={styles.text}>{Object.keys(content)[0].includes("OXX") ? "Entrega tu envío en la caja para que la paquetería que elegiste pueda pasar a buscarlo." : "Acude a una sucursal de la paquetería que elegiste. No podrás dejar tu paquete en esta tienda."}</h3>
                 </div>
             )
         case "resumeSenderReceiver":
